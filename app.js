3125de05b7f0bc4ec5186e71bbbc95b553ed5714
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2022-12-12 15:48:10 +0700

    add functions for start, next, check, and hint

--- app.js
+++ app.js
@@ -1,50 +1,83 @@
 // Mathapalooza 2000
 
-// global variables
-const questions = {}; 
+// GLOBAL VARIABLES
+const questions = {
+  question: ["248 + (-1027) = ?", "818 - (-1021) = ?", "-321 - 857 = ?", "What is 624 divided by 6?", "What is 56 divided by -8?", "What is -42 divided by -7?", "What is 4/5 as a decimal?", "What is the cube root of 512?", "What is 9 squared?", "What is 1 5/8 as a decimal?"],
+  hint: ["HINT: Adding a negative number is the same as subtracting a positive number.", "HINT: Subtractiing a negative number is the same as adding a positive number.", "HINT: Subtracting a positive number from a negative number is the same as adding two negative numbers.", "HINT: Use the Long Division algorithm to compute the quotient.", "HINT: A positive number multiplied by or divided by a negative number will result in a negative number.", "HINT: A negative number multiplied by or divided by a negative number will result in a negative number.", "HINT: Use the Long Division algorithm to compute the quotient. Remember to divided the denominator into the numerator.", "HINT: What number times itself three times is 512?", "HINT: To square a number you must multiply it by itself.", "HINT: Use the Long Division algorithm to compute the quotient. Remember to divided the denominator into the numerator."],
+  answer: [-779, 1839, -908, 104, -7, 6, 0.8, 8, 81, 1.625]
+}
+var points = 0;
+let randomNum = Math.floor( Math.random() * questions["question"].length);
+let answerCorrect = false;
+let hintGiven = false;
+let gameOver = false;
+let count = 0;
+
+// buttons 
 const startBtn = document.getElementById("start-game");
 const checkBtn = document.getElementById("check-btn");
 const hintBtn = document.getElementById("hint-btn");
-const nextBtn = document.getElementById("next-btn")
+const nextBtn = document.getElementById("next-btn");
 
-// event listeners
+// page elements
+const gamezoneTitle = document.getElementById("gamezone__title");
+const gamezoneQuestion = document.getElementById("gamezone__question")
+const questionTxt = document.getElementById("gamezone__question-text");
+const pointsTxt = document.getElementById("gamezone__points");
+const feedbackTxt = document.getElementById("gamezone__question--feedback");
+const inputAnswer = document.getElementById("gamezone__question--input");
+
+// EVENT LISTENERS
 startBtn.addEventListener("click", startGame);
 checkBtn.addEventListener("click", checkAnswer);
 hintBtn.addEventListener("click", hint);
 nextBtn.addEventListener("click", nextQuestion);
 
-// functions 
+// FUNCTIONS 
 function startGame() {
-  console.log("the start button was clicked");
-
-  // delete text in the div id="gamezone__title"
-
-  // set the text in the div id="gamezone__question"
-  // diplay question text based on randomly generator number 
+  randomNum = Math.floor( Math.random() * questions["question"].length);
+  gamezoneTitle.innerText = "";
+  gamezoneQuestion.setAttribute("style", "");
+  pointsTxt.innerText = `Points: ${points}`;
+  questionTxt.innerText = questions["question"][randomNum];
+  inputAnswer.value = "";
 };
 
 function checkAnswer() {
-  console.log("the check button was clicked");
-
-  // compare input text with answer in obj questions (use same randomNum as index to check)
-  // use if statement to display feedback text
-  // if correct, display "correct", add points, and display nextBtn
-  // if incorrect, display "try again!" and deduct points
-
-
+  if (gameOver == false && answerCorrect == false && count < 2) {
+    if (inputAnswer.value == questions["answer"][randomNum]) {
+      answerCorrect = true;
+      feedbackTxt.innerText = "Correct!";
+      nextBtn.setAttribute("style", "");
+      points += 10;
+      pointsTxt.innerHTML = `Points: ${points}`;
+    } else {
+      points -= 5;
+      pointsTxt.innerHTML = `Points: ${points}`;
+      feedbackTxt.innerText = "Try again!";
+    };
+  } else {
+    feedbackTxt.innerText = `Congrat! You won ${points} points!`;
+  };
 };
 
 function hint() {
-  console.log("the hint button was clicked");
-
-  // if clicked, display corresponding hint based on randomNum in the feedback text, and deduct point
-
+  if (answerCorrect === false && hintGiven === false && gameOver === false) {
+    hintGiven = true;
+    points -= 2;
+    pointsTxt.innerHTML = `Points: ${points}`;
+    feedbackTxt.innerText = questions["hint"][randomNum];
+  };
 };
 
 function nextQuestion() {
-  console.log("the next button was clicked");
-
-  // set the text in the div id="gamezone__question"
-  // diplay question text based on randomly generator number 
+  count += 1;
+  answerCorrect = false;
+  hintGiven = false;
+  inputAnswer.value = "";
+  randomNum = Math.floor( Math.random() * questions["question"].length);
+  nextBtn.setAttribute("style", "visibility: hidden;");
+  feedbackTxt.innerText = "";
+  questionTxt.innerText = questions["question"][randomNum];
   
 };
--- questions.md
+++ questions.md
@@ -1,36 +1,50 @@
+# Mathapalooza 2000
+
 ## Questions 
 
-1. 
-2. 
-3. 
-4. 
-5. 
-6. 
-
-## Hints
-
-1. 
-2. 
-3. 
-4. 
-5. 
-6. 
+1. "248 + (-1027) = ?"
+2. "818 - (-1021) = ?"
+3. "-321 - 857 = ?"
+4. "What is 624 divided by 6?"
+5. "What is 56 divided by -8?"
+6. "What is -42 divided by -7?"
+7. "What is 4/5 as a decimal?"
+8. "What is the cube root of 512?"
+9. "What is 9 squared?"
+10. "What is 1 5/8 as a decimal?"
 
 ## Answers 
 
-1. 
-2. 
-3. 
-4. 
-5. 
-6. 
+1. -779
+2. 1839
+3. -908
+4. 104
+5. -7
+6. 6
+7. 0.8
+8. 8
+9. 81
+10. 1.625
+
+## Hints
+
+1. "HINT: Adding a negative number is the same as subtracting a positive number." 
+2. "HINT: Subtractiing a negative number is the same as adding a positive number."
+3. "HINT: Subtracting a positive number from a negative number is the same as adding two negative numbers."
+4. "HINT: Use the Long Division algorithm to compute the quotient."
+5. "HINT: A positive number multiplied by or divided by a negative number will result in a negative number."
+6. "HINT: A negative number multiplied by or divided by a negative number will result in a negative number."
+7. "HINT: Use the Long Division algorithm to compute the quotient. Remember to divided the denominator into the numerator."
+8. "HINT: What number times itself three times is 512?"
+9. "HINT: To square a number you must multiply it by itself."
+10. "HINT: Use the Long Division algorithm to compute the quotient. Remember to divided the denominator into the numerator."
 
 ## JS Object 
 
 ```
-questions = {
-  question: [],
-  hint: [],
-  answer: []
+const questions = {
+  question: ["248 + (-1027) = ?", "818 - (-1021) = ?", "-321 - 857 = ?", "What is 624 divided by 6?", "What is 56 divided by -8?", "What is -42 divided by -7?", "What is 4/5 as a decimal?", "What is the cube root of 512?", "What is 9 squared?", "What is 1 5/8 as a decimal?"],
+  hint: ["HINT: Adding a negative number is the same as subtracting a positive number.", "HINT: Subtractiing a negative number is the same as adding a positive number.", "HINT: Subtracting a positive number from a negative number is the same as adding two negative numbers.", "HINT: Use the Long Division algorithm to compute the quotient.", "HINT: A positive number multiplied by or divided by a negative number will result in a negative number.", "HINT: A negative number multiplied by or divided by a negative number will result in a negative number.", "HINT: Use the Long Division algorithm to compute the quotient. Remember to divided the denominator into the numerator.", "HINT: What number times itself three times is 512?", "HINT: To square a number you must multiply it by itself.", "HINT: Use the Long Division algorithm to compute the quotient. Remember to divided the denominator into the numerator."],
+  answer: [-779, 1839, -908, 104, -7, 6, 0.8, 8, 81, 1.625]
 }
 ```
